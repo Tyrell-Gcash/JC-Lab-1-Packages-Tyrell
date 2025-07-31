e867a6bd438520aaf8270b09d6c897faa4118771
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-07-31 14:06:44 +0800

    Lab Done - Teppang

---
+++ Readme.md
@@ -0,0 +1,1 @@
+<img src="Teppang-JavaPackages.png">
--- src/main/java/org/example/MainLab.java
+++ src/main/java/org/example/MainLab.java
@@ -1,17 +1,21 @@
 package org.example;
 
-import org.example.packageone.ClassOne;
-import org.example.packagetwo.ClassTwo;
-import org.example.packagethree.ClassThree;
-
 import static java.lang.System.out;
+
+import org.example.packageone.ClassOne;
+import org.example.packagethree.ClassThree;
+import org.example.packagetwo.ClassTwo;
 
 public class MainLab {
     public static void main(String[] args) {
 
-        println(ClassOne.greet());
-        println(ClassTwo.greet());
-        println(ClassThree.greet());
+        ClassOne classOne = new ClassOne();
+        ClassTwo classTwo = new ClassTwo();
+        ClassThree classThree = new ClassThree();
+
+        out.println(classOne.greet());
+        out.println(classTwo.greet());
+        out.println(classThree.greet());
       
     }
 }
